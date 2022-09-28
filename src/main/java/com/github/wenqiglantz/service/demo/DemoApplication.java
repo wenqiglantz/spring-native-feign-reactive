9a1be26a4f93c670e9af120277a5a17c36cb477a
wglantz <wglantz@arisglobal.com>
2022-09-28 08:35:19 -0400

    added config json files generated by tracing agent

--- src/main/java/com/github/wenqiglantz/service/demo/DemoApplication.java
+++ src/main/java/com/github/wenqiglantz/service/demo/DemoApplication.java
@@ -1,12 +1,15 @@
 package com.github.wenqiglantz.service.demo;
 
-import com.github.wenqiglantz.service.demo.democlient.DemoClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import reactivefeign.spring.config.EnableReactiveFeignClients;
 
 @SpringBootApplication
-@EnableReactiveFeignClients(clients = {DemoClient.class})
+@EnableReactiveFeignClients
+@ComponentScan({
+		"reactivefeign.spring.config"
+})
 public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
